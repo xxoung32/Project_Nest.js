46331115baca1ba351f9107eb6f22e5884b27df3
Ayoung <say199532@gmail.com>
2023-12-11 16:44:31 +0900

    typeorm 연결

---
+++ data-source.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
+
+export const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  port: Number(process.env.TYPEORM_PORT),
+  entities: [
+    path.join(__dirname, 'src/entities/**/*.entity{.ts,.js}'), 
+    path.join(__dirname, 'dist/entities/**/*.entity{.ts,.js}'),
+  ],
+});
