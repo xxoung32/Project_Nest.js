83c4b7682c2d7b679c58783e7eb59aec37236580
Ayoung <say199532@gmail.com>
2023-12-24 16:44:03 +0900

    fix: 좋아요 생성시 param과 body의 postId 불일치 에러 처리

--- src/post-likes/post-likes.controller.ts
+++ src/post-likes/post-likes.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Body,
+  Param,
+  ParseIntPipe,
+  BadRequestException
+} from '@nestjs/common';
+import { PostLikesService } from './post-likes.service';
+import { postLikes } from 'src/entities/post_likes.entity';
+import { createPostLikeDto } from './dto/createPostLike.dto';
 
-@Controller('post-likes')
-export class PostLikesController {}
+@Controller('postLikes')
+export class PostLikesController {
+  constructor(private postLikesService: PostLikesService) {}
+
+  @Post(':postId')
+  @UsePipes(ValidationPipe)
+  createPost(
+    @Body() createPostLikeDto: createPostLikeDto,
+    @Param('postId', ParseIntPipe) postId: number,
+  ): Promise<postLikes> {
+    if (postId !== createPostLikeDto.postId) {
+      throw new BadRequestException('postId in URL and Body do not match');
+    }
+    return this.postLikesService.createPostLike(createPostLikeDto);
+  }
+}
--- src/post-likes/post-likes.service.ts
+++ src/post-likes/post-likes.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { postLikesRepository } from './post-likes.repository';
+import { PostRepository } from 'src/posts/posts.repository';
 import { createPostLikeDto } from './dto/createPostLike.dto';
 import { postLikes } from 'src/entities/post_likes.entity';
 
@@ -9,8 +10,19 @@ export class PostLikesService {
   constructor(
     @InjectRepository(postLikesRepository)
     private postLikesRepository: postLikesRepository,
+    private PostRepository: PostRepository,
   ) {}
-  async createPostLike(createPostLikeDto: createPostLikeDto): Promise<postLikes> {
-    const existingPost = await this.PostRepository.findOne
+  async createPostLike(
+    createPostLikeDto: createPostLikeDto,
+  ): Promise<postLikes> {
+    const existingPost = await this.PostRepository.findOne({
+      where: { id: createPostLikeDto.postId },
+    });
+    if (!existingPost) {
+      throw new NotFoundException(
+        `ID ${createPostLikeDto.postId}에 해당하는 게시글을 찾을 수 없거나 게시되지 않았습니다.`,
+      );
+    }
+    return this.postLikesRepository.createPostLike(createPostLikeDto);
   }
 }
